f54fc9b50541fc80d8aab323eb9c532556ef6ca1
AncaGr <87362802+AncaGr@users.noreply.github.com>
2021-07-13 10:47:26 +0200

    Python script
    Countdown from 100 to 1 and printing either numbers or strings

---
+++ main.py
@@ -0,0 +1,10 @@
+
+for i in range(100, 0, -1):
+    if i % 5 == 0 and i % 3 == 0:
+        print("Testing");
+    elif i % 3 == 0:
+        print("Software");
+    elif i % 5 == 0:
+        print("Agile");
+    else:
+        print(i);
